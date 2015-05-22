1612c03c38783a269b6e254bc409a72306f82eb4
Ed Hager <ehager@sitepen.com>
2015-05-22 16:11:10 -0500

    Update test to match suggested recipe.

--- tests/functional/basicAmdLoading.ts
+++ tests/functional/basicAmdLoading.ts
@@ -1,18 +1,26 @@
 import * as assert from 'intern/chai!assert';
 import * as registerSuite from 'intern!object';
+import * as Suite from 'intern/lib/Suite';
+import * as Command from 'leadfoot/Command';
 
 import pollUntil = require('intern/dojo/node!leadfoot/helpers/pollUntil');
+
+function executeTest(suite: Suite, htmlTestPath: string, testFn: (result: any) => void, timeout = 5000): Command<any> {
+	return suite.remote
+		.get((<any>require).toUrl(htmlTestPath))
+		.then(pollUntil<any>(function () {
+			return (<any>window).loaderTestResults;
+		}, null, timeout), undefined)
+		.then(testFn, function () {
+			throw new Error('loaderTestResult was not set.');
+		});
+}
 
 registerSuite({
 	name: 'basic AMD loading',
 	'simple test'() {
-		return this.remote
-			.get((<any>require).toUrl('./basicAmdLoading.html'))
-			.then(pollUntil(function () {
-				return (<any>window).testMessage;
-			}, null, 5000))
-			.then(function (message: string) {
-				assert.strictEqual(message, 'Message from AMD app.');
+		return executeTest(this, './basicAmdLoading.html', function (results: any) {
+				assert.strictEqual(results.message, 'Message from AMD app.');
 			});
 	}
 });
