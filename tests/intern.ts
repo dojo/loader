93875f0830dbea106cd065f86fb26476aacdac8e
Kitson Kelly <me@kitsonkelly.com>
2016-05-08 14:44:50 +0100

    Update intern.ts

--- tests/intern.ts
+++ tests/intern.ts
@@ -19,12 +19,12 @@ export var capabilities = {
 // OnDemand. Options that will be permutated are browserName, version, platform, and platformVersion; any other
 // capabilities options specified for an environment will be copied as-is
 export var environments = [
-	/*{ browserName: 'internet explorer', version: [ '10.0', '11.0' ], platform: 'Windows 7' },*/
-	{ browserName: 'microsoftedge', platform: 'Windows 10' }/*,
+	{ browserName: 'internet explorer', version: [ '10.0', '11.0' ], platform: 'Windows 7' },
+	{ browserName: 'microsoftedge', platform: 'Windows 10' },
 	{ browserName: 'firefox', platform: 'Windows 10' },
 	{ browserName: 'chrome', platform: 'Windows 10' },
 	{ browserName: 'safari', version: '9', platform: 'OS X 10.11' },
-	{ browserName: 'android', platform: 'Linux', version: '4.4', deviceName: 'Google Nexus 7 HD Emulator' }*/
+	{ browserName: 'android', platform: 'Linux', version: '4.4', deviceName: 'Google Nexus 7 HD Emulator' }
 ];
 
 // Maximum number of simultaneous integration tests that should be executed on the remote WebDriver service
