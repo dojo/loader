d6d42f5f09a098abcfdefcfe44b963c35d24d7fc
Kitson Kelly <me@kitsonkelly.com>
2016-05-17 08:36:53 +0100

    Remove /typings

--- typings/main.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference path="main/ambient/node/index.d.ts" />
-
-/* Simple Nashorn Typings */
-declare var arguments: any;
-declare var load: any;
-declare var Packages: any;
-
-declare var global: any;
