49155e4ff314136f760486477a82bc2204fc9908
Rory Mulligan <rorticus@gmail.com>
2017-01-27 09:51:33 -0500

    Adding test for undefined modules, issue-16

--- tests/unit/require.ts
+++ tests/unit/require.ts
@@ -631,43 +631,51 @@ registerSuite({
 			dfd.reject('Loading undefined module should throw an error');
 		});
 	},
-	'recursive undef'(this: any) {
-		let dfd = this.async(DEFAULT_TIMEOUT);
+	'recurisve undef': {
+		'dependencies are unloaded'(this: any) {
+			let dfd = this.async(DEFAULT_TIMEOUT);
 
-		global.require.config({
-			packages: [
-				{
-					name: 'recursive',
-					location: './_build/tests/common/recursive'
-				}
-			]
-		});
-
-		function checkForUndef(mod: string): boolean {
-			try {
-				global.require(mod);
-			} catch (error) {
-				if (error.message.indexOf(mod) !== -1) {
-					return true;
-				}
-			}
-			return false;
-		}
-
-		global.require([
-			'recursive/a'
-		], function () {
-			global.require.undef('recursive/a', true);
-			const deps: string[] = ['recursive/a', 'recursive/b', 'recursive/c', 'recursive/d', 'recursive/e'];
-			const passed: boolean = deps.every(function (mod: string) {
-				return checkForUndef(mod);
+			global.require.config({
+				packages: [
+					{
+						name: 'recursive',
+						location: './_build/tests/common/recursive'
+					}
+				]
 			});
-			if (passed) {
-				dfd.resolve();
-			} else {
-				dfd.reject('not all dependencies were undefined');
+
+			function checkForUndef(mod: string): boolean {
+				try {
+					global.require(mod);
+				}
+				catch (error) {
+					if (error.message.indexOf(mod) !== -1) {
+						return true;
+					}
+				}
+				return false;
 			}
-		});
+
+			global.require([
+				'recursive/a'
+			], function () {
+				global.require.undef('recursive/a', true);
+				const deps: string[] = [ 'recursive/a', 'recursive/b', 'recursive/c', 'recursive/d', 'recursive/e' ];
+				const passed: boolean = deps.every(function (mod: string) {
+					return checkForUndef(mod);
+				});
+				if (passed) {
+					dfd.resolve();
+				}
+				else {
+					dfd.reject('not all dependencies were undefined');
+				}
+			});
+		},
+
+		'modules without dependencies work as expected'() {
+			global.require.undef('invalid-module', true);
+		}
 	},
 
 	plugin: {
