d3a7cbebcc64674f1fa84ed6a7fe541242009cf1
Anthony Gubler <agubler@gmail.com>
2017-03-20 14:48:37 +0000

    Remove dependency on tslint and grunt-tslint (#118)
    * remove dependency on tslint and grunt-tslint
    
    * clean up rules in preperation for tslint 4+
    
    * Fix lint errors preemptively for tslint version 4

--- tests/intern.ts
+++ tests/intern.ts
@@ -1,7 +1,7 @@
-export var proxyPort = 9000;
+export const proxyPort = 9000;
 
 // A fully qualified URL to the Intern proxy
-export var proxyUrl = 'http://localhost:9000/';
+export const proxyUrl = 'http://localhost:9000/';
 
 // Default desired capabilities for all environments. Individual capabilities can be overridden by any of the
 // specified browser environments in the `environments` array below as well. See
@@ -9,7 +9,7 @@ export var proxyUrl = 'http://localhost:9000/';
 // https://saucelabs.com/docs/additional-config#desired-capabilities for Sauce Labs capabilities.
 // Note that the `build` capability will be filled in with the current commit ID from the Travis CI environment
 // automatically
-export var capabilities = {
+export const capabilities = {
 	project: 'Dojo 2',
 	name: '@dojo/loader',
 	fixSessionCapabilities: false
@@ -18,7 +18,7 @@ export var capabilities = {
 // Browsers to run integration testing against. Note that version numbers must be strings if used with Sauce
 // OnDemand. Options that will be permutated are browserName, version, platform, and platformVersion; any other
 // capabilities options specified for an environment will be copied as-is
-export var environments = [
+export const environments = [
 	{ browserName: 'internet explorer', version: [ '10.0', '11.0' ], platform: 'Windows 7' },
 	{ browserName: 'microsoftedge', platform: 'Windows 10' },
 	{ browserName: 'firefox', platform: 'Windows 10' },
@@ -28,10 +28,10 @@ export var environments = [
 ];
 
 // Maximum number of simultaneous integration tests that should be executed on the remote WebDriver service
-export var maxConcurrency = 4;
+export const maxConcurrency = 4;
 
 // Name of the tunnel class to use for WebDriver tests
-export var tunnel = 'SauceLabsTunnel';
+export const tunnel = 'SauceLabsTunnel';
 
 export const loaders = {
 	'host-browser': 'node_modules/@dojo/loader/loader.js',
@@ -40,7 +40,7 @@ export const loaders = {
 
 // Configuration options for the module loader; any AMD configuration options supported by the specified AMD loader
 // can be used here
-export var loaderOptions = {
+export const loaderOptions = {
 	// Packages that should be registered with the loader in each testing environment
 	packages: [
 		{ name: 'src', location: '_build/src' },
@@ -51,10 +51,10 @@ export var loaderOptions = {
 };
 
 // Non-functional test suite(s) to run in each browser
-export var suites = [ 'tests/unit/all' ];
+export const suites = [ 'tests/unit/all' ];
 
 // Functional test suite(s) to run in each browser once non-functional tests are completed
-export var functionalSuites = [ 'tests/functional/all' ];
+export const functionalSuites = [ 'tests/functional/all' ];
 
 // A regular expression matching URLs to files that should not be included in code coverage analysis
-export var excludeInstrumentation = /(?:node_modules|bower_components|tests)[\/\\]/;
+export const excludeInstrumentation = /(?:node_modules|bower_components|tests)[\/\\]/;
